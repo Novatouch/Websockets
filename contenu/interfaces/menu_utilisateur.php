e0096072b6722001f333f7033354ee32fcbe6a7a
Remi Maison <maison.remi@orange.fr>
2014-06-25 11:00:06 +0200

    Update menu_utilisateur.php

--- contenu/interfaces/menu_utilisateur.php
+++ contenu/interfaces/menu_utilisateur.php
@@ -1,17 +1,5 @@
 <html lang="fr">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="assets/ico/favicon.ico">
-        
-        <!-- Bootstrap core CSS -->
-        <link href="../../css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="../../css/offcanvas.css" rel="stylesheet">
     </head>
     <body>
         <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
