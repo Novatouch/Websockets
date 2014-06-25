b87b34044c855f830a07b229e71f732631b9e3a5
Novatouch <mail@gmail.com>
2014-06-25 07:24:14 +0200

    modification MCD

--- sql/creationBase.sql
+++ sql/creationBase.sql
@@ -8,10 +8,10 @@ DROP DATABASE webappbd;
 
 -- DROP DATABASE "BD_PROJET";
 CREATE DATABASE webappbd
-  WITH OWNER = postgres
-      ENCODING = 'UTF8'
-      LC_COLLATE = 'fr_FR.UTF-8'
-      LC_CTYPE = 'fr_FR.UTF-8';
+WITH OWNER = postgres
+ENCODING = 'UTF8'
+LC_COLLATE = 'fr_FR.UTF-8'
+LC_CTYPE = 'fr_FR.UTF-8';
 
 \c webappbd postgres
 
@@ -33,8 +33,8 @@ DROP TABLE IF EXISTS questionProposition CASCADE;
 
 CREATE TABLE IF NOT EXISTS webapp.role
 (
-	rol_id SERIAL CONSTRAINT pk_rol_id PRIMARY KEY,
-	rol_nom VARCHAR(30)
+    rol_id SERIAL CONSTRAINT pk_rol_id PRIMARY KEY,
+    rol_nom VARCHAR(30)
 );
 
 --***********INSERTION TABLE role ******************************************
@@ -46,15 +46,15 @@ INSERT INTO webapp.role (rol_nom) VALUES ('utilisateur');
 
 CREATE TABLE IF NOT EXISTS webapp.utilisateur
 (
-	util_id SERIAL CONSTRAINT pk_util_id PRIMARY KEY,
-	util_identifiant VARCHAR(30),
-	util_prenom VARCHAR(30),
-	util_nom VARCHAR(30),
-	util_pass CHAR(128),
-	util_session_token CHAR(64),
-	util_role_id INT,
-	CONSTRAINT un_util_identifiant UNIQUE(util_identifiant),
-	CONSTRAINT fk_util_role_id FOREIGN KEY(util_role_id) REFERENCES webapp.role(rol_id)
+    util_id SERIAL CONSTRAINT pk_util_id PRIMARY KEY,
+    util_identifiant VARCHAR(30),
+    util_prenom VARCHAR(30),
+    util_nom VARCHAR(30),
+    util_pass CHAR(128),
+    util_session_token CHAR(64),
+    util_role_id INT,
+    CONSTRAINT un_util_identifiant UNIQUE(util_identifiant),
+    CONSTRAINT fk_util_role_id FOREIGN KEY(util_role_id) REFERENCES webapp.role(rol_id)
 );
 
 
@@ -65,44 +65,10 @@ INSERT INTO webapp.utilisateur (util_identifiant, util_prenom, util_nom, util_pa
 
 
 
-
-CREATE TABLE webapp.proposition
-(
-	pro_id SERIAL CONSTRAINT pk_pro_id PRIMARY KEY,
-	pro_texte VARCHAR(100)
-);
-
-
-INSERT INTO webapp.proposition (pro_texte) VALUES ('oui');
-INSERT INTO webapp.proposition (pro_texte) VALUES ('non');
-INSERT INTO webapp.proposition (pro_texte) VALUES ('Je ne sais pas mais je réponds oui');
-INSERT INTO webapp.proposition (pro_texte) VALUES ('Je n''ai pas d''avis sur la question');
-
-
-
-CREATE TABLE IF NOT EXISTS webapp.repondre
-(
-	reponse_utilisateur_id INT,
-	reponse_proposition_id INT,
-	CONSTRAINT pk_ag_rep_util_id_rep_pro_id PRIMARY KEY (reponse_utilisateur_id, reponse_proposition_id),
-	CONSTRAINT fk_reponse_utilisateur_id FOREIGN KEY(reponse_utilisateur_id) REFERENCES webapp.utilisateur(util_id),
-	CONSTRAINT fk_reponse_proposition_id FOREIGN KEY(reponse_proposition_id) REFERENCES webapp.proposition(pro_id)
-);
-
-
---***********INSERTION TABLE repondre******************************************
-
-INSERT INTO webapp.repondre (reponse_utilisateur_id,reponse_proposition_id) VALUES (1, 1);
-INSERT INTO webapp.repondre (reponse_utilisateur_id,reponse_proposition_id) VALUES (1, 2);
-
-
-
-
-
 CREATE TABLE IF NOT EXISTS webapp.statut
 (
-	sta_id SERIAL CONSTRAINT pk_sta_id PRIMARY KEY,
-	sta_nom VARCHAR(100)
+    sta_id SERIAL CONSTRAINT pk_sta_id PRIMARY KEY,
+    sta_nom VARCHAR(100)
 );
 
 --***********INSERTION TABLE statut******************************************
@@ -116,17 +82,17 @@ INSERT INTO webapp.statut (sta_nom) VALUES ('fermé');
 
 CREATE TABLE IF NOT EXISTS webapp.sondage
 (
-	son_id SERIAL CONSTRAINT pk_son_id PRIMARY KEY,
-	son_theme VARCHAR(100),
-	son_texte VARCHAR(100),
-	son_statut_id INT,
+    son_id SERIAL CONSTRAINT pk_son_id PRIMARY KEY,
+    son_theme VARCHAR(100),
+    son_presentation VARCHAR(100),
+    son_statut_id INT,
     sond_date_debut TIME with time zone,
     sond_date_fin TIME with time zone,
-	CONSTRAINT fk_son_statut_id FOREIGN KEY(son_statut_id) REFERENCES webapp.statut(sta_id)
+    CONSTRAINT fk_son_statut_id FOREIGN KEY(son_statut_id) REFERENCES webapp.statut(sta_id)
 );
 
 --***********INSERTION TABLE sondage******************************************
-INSERT INTO webapp.sondage (son_theme, son_texte, son_statut_id) VALUES ('education','L''éductation nationnale','2');
+INSERT INTO webapp.sondage (son_theme, son_presentation, son_statut_id) VALUES ('education','L''éductation nationnale','2');
 
 
 
@@ -134,60 +100,101 @@ INSERT INTO webapp.sondage (son_theme, son_texte, son_statut_id) VALUES ('educat
 
 CREATE TABLE IF NOT EXISTS webapp.question
 (
-	ques_id SERIAL CONSTRAINT pk_ques_id PRIMARY KEY,
-	ques_texte VARCHAR(200),
-	ques_sondage_id INT,
-	CONSTRAINT fk_ques_sondage_id FOREIGN KEY(ques_sondage_id) REFERENCES webapp.sondage(son_id)
+    ques_id SERIAL CONSTRAINT pk_ques_id PRIMARY KEY,
+    ques_texte VARCHAR(200),
+    ques_sondage_id INT,
+    CONSTRAINT fk_ques_sondage_id FOREIGN KEY(ques_sondage_id) REFERENCES webapp.sondage(son_id)
 );
 
 --***********INSERTION TABLE question******************************************
 INSERT INTO webapp.question (ques_texte, ques_sondage_id) VALUES ('Penser vous que la qualité de l''enseignement à baissé depuis l''année 1920', 1);
 
-
-
-
-CREATE TABLE IF NOT EXISTS webapp.questionProposition
+CREATE TABLE webapp.proposition
 (
-	quepro_question_id INT,
-	quepro_propostion_id INT,
-	quepro_nombre_votants INT DEFAULT 0,
-	CONSTRAINT pk_ag_quepro_question_id_quepro_propostion_id PRIMARY KEY (quepro_question_id, quepro_propostion_id),
-	CONSTRAINT fk_quepro_question_id FOREIGN KEY(quepro_question_id) REFERENCES webapp.question(ques_id),
-	CONSTRAINT fk_quepro_propostion_id FOREIGN KEY(quepro_propostion_id) REFERENCES webapp.proposition(pro_id)
+    pro_id SERIAL CONSTRAINT pk_pro_id PRIMARY KEY,
+    pro_texte VARCHAR(100),
+    pro_question_id INT,
+    pro_nombre_votants INT DEFAULT 0,
+    CONSTRAINT fk_pro_question_id FOREIGN KEY(pro_question_id) REFERENCES webapp.question(ques_id)
 );
 
---***********INSERTION TABLE questionProposition******************************************
-INSERT INTO webapp.questionProposition (quepro_question_id, quepro_propostion_id, quepro_nombre_votants) VALUES (1, 1,1);
-INSERT INTO webapp.questionProposition (quepro_question_id, quepro_propostion_id, quepro_nombre_votants) VALUES (1, 2,1);
-INSERT INTO webapp.questionProposition (quepro_question_id, quepro_propostion_id) VALUES (1, 3);
-INSERT INTO webapp.questionProposition (quepro_question_id, quepro_propostion_id) VALUES (1, 4);
+
+INSERT INTO webapp.proposition (pro_texte, pro_question_id, pro_nombre_votants) VALUES ('oui', 1, 1);
+INSERT INTO webapp.proposition (pro_texte, pro_question_id, pro_nombre_votants) VALUES ('non', 1, 1);
+INSERT INTO webapp.proposition (pro_texte, pro_question_id) VALUES ('Je ne sais pas mais je réponds oui', 1);
+INSERT INTO webapp.proposition (pro_texte, pro_question_id) VALUES ('Je n''ai pas d''avis sur la question', 1);
+
+
+
+CREATE TABLE IF NOT EXISTS webapp.repondre
+(
+    rep_utilisateur_id INT,
+    rep_question_id INT,
+    rep_proposition_id INT,
+    CONSTRAINT pk_ag_rep_util_id_rep_pro_id PRIMARY KEY (rep_utilisateur_id, rep_question_id),
+    CONSTRAINT fk_rep_utilisateur_id FOREIGN KEY(rep_utilisateur_id) REFERENCES webapp.utilisateur(util_id),
+    CONSTRAINT fk_rep_proposition_id FOREIGN KEY(rep_proposition_id) REFERENCES webapp.proposition(pro_id),
+    CONSTRAINT fk_rep_question_id FOREIGN KEY(rep_question_id) REFERENCES webapp.question(ques_id)
+);
+
+
+--***********INSERTION TABLE repondre******************************************
+
+INSERT INTO webapp.repondre (rep_utilisateur_id, rep_question_id, rep_proposition_id) VALUES (1, 1, 1);
+INSERT INTO webapp.repondre (rep_utilisateur_id, rep_question_id, rep_proposition_id) VALUES (2, 1, 2);
+
+
 
 
 --***********Vues ******************************************
 CREATE OR REPLACE VIEW webapp.authentification (id, identifiant, prenom, nom, pass, token , role) AS
-	SELECT u.util_id, u.util_identifiant, u.util_prenom, u.util_nom, u.util_pass, u.util_session_token, r.rol_nom
-	FROM webapp.utilisateur u, webapp.role r
-	WHERE u.util_role_id = r.rol_id;
+SELECT u.util_id, u.util_identifiant, u.util_prenom, u.util_nom, u.util_pass, u.util_session_token, r.rol_nom
+FROM webapp.utilisateur u, webapp.role r
+WHERE u.util_role_id = r.rol_id;
 
 CREATE OR REPLACE VIEW webapp.authentification_insertion (id, token) AS
-	SELECT util_id, util_session_token 
-	FROM webapp.utilisateur;
+SELECT util_id, util_session_token 
+FROM webapp.utilisateur;
+
+
+CREATE OR REPLACE VIEW webapp.vue_repondre_insertion  (utilisateur_id, question_id, proposition_id) AS
+SELECT rep_utilisateur_id,  rep_question_id, rep_proposition_id
+FROM webapp.repondre;
 
 CREATE OR REPLACE VIEW webapp.vue_sondage (json) AS
 SELECT array_to_json(array_agg(row_to_json(t))) AS json
 FROM (
     SELECT so.son_id AS id , so.son_theme AS theme
-	FROM webapp.sondage so, webapp.statut st
+    FROM webapp.sondage so, webapp.statut st
     WHERE so.son_statut_id = st.sta_id
 ) t;
 
 
 CREATE OR REPLACE VIEW webapp.resultat (identifiant_sondage, theme_sondage, presentation_sondage, identifiant_question, texte_question, identifant_proposition, texte_proposition, nombre_votants, pourcentage) AS
-	SELECT s.son_id, s.son_theme, s.son_texte, q.ques_id, q.ques_texte, p.pro_id, p.pro_texte, qp.quepro_nombre_votants, 100*qp.quepro_nombre_votants/(sum(qp.quepro_nombre_votants) OVER (PARTITION BY qp.quepro_question_id))
-	FROM webapp.sondage s, webapp.question q, webapp.questionProposition qp, webapp.proposition p
-	WHERE s.son_id = q.ques_sondage_id
-	AND q.ques_id = qp.quepro_question_id 
-	AND qp.quepro_propostion_id = p. pro_id;
+SELECT s.son_id, s.son_theme, s.son_presentation, q.ques_id, q.ques_texte, p.pro_id, p.pro_texte, p.pro_nombre_votants, 100*p.pro_nombre_votants/(sum(p.pro_nombre_votants) OVER (PARTITION BY p.pro_question_id))
+FROM webapp.sondage s, webapp.question q, webapp.proposition p
+WHERE s.son_id = q.ques_sondage_id
+AND q.ques_id = p.pro_question_id;
+
+
+----********* TRIGGER ***********************************************
+CREATE OR REPLACE FUNCTION mise_a_jour_score() RETURNS trigger AS $$
+BEGIN
+    UPDATE webapp.proposition SET pro_nombre_votants=pro_nombre_votants+1
+    WHERE NEW.rep_proposition_id = pro_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--********* TRIGGER ***********************************************
+CREATE TRIGGER vue_repondre_insertion
+AFTER INSERT ON webapp.repondre
+FOR EACH ROW
+EXECUTE PROCEDURE mise_a_jour_score();
+
+
+
 
 
 --********* Role ***********************************************
